d59206d2c60ff8c53d3c2e4cd461137591ee9016
DeathFull <rudymonteiro44@gmail.com>
2019-12-04 00:08:49 +0100

    Ajout des obstacles supérieur (part 1)

--- pacman.py
+++ pacman.py
@@ -24,7 +24,7 @@ def collision(rectA, rectB):
     return True
 
 
-def collisionBackground(joueur, collision):
+def collisionRadius(joueur, collision):
     if math.sqrt((collision.x - joueur.x) ** 2 + (collision.y - joueur.y) ** 2) <= 28:
         return True
     else:
@@ -34,47 +34,158 @@ def collisionBackground(joueur, collision):
 pygame.init()
 
 fenetre = pygame.display.set_mode((600, 600))
-background = pygame.image.load("map.jpg")
-fenetre.blit(background, (0, 0))
 pygame.display.set_caption("PacMan")
 
-perso = Player((20, 20), pygame.image.load("perso.png"))
+ch = Sprite()
+ch.image = pygame.image.load("contour_haut.png")
+ch.image = pygame.transform.scale(ch.image, (600, 271))
+ch.rect = ch.image.get_rect()
+ch.mask = pygame.mask.from_surface(ch.image)
+
+cb = Sprite()
+cb.image = pygame.image.load("contour_bas.png")
+cb.image = pygame.transform.scale(cb.image, (600, 271))
+cb.rect = cb.image.get_rect()
+cb.rect.topleft = (0, 329)
+cb.mask = pygame.mask.from_surface(cb.image)
+
+start = Sprite()
+start.image = pygame.image.load("start.png")
+start.rect = start.image.get_rect()
+start.rect.topleft = (234, 242)
+start.mask = pygame.mask.from_surface(start.image)
+
+piece2 = Sprite()
+piece2.image = pygame.image.load("piece2.png")
+piece2.rect = piece2.image.get_rect()
+piece2.rect.topleft = (389, 300)
+piece2.mask = pygame.mask.from_surface(piece2.image)
+
+piece6 = Sprite()
+piece6.image = pygame.image.load("piece6.png")
+piece6.rect = piece6.image.get_rect()
+piece6.rect.topleft = (216, 126)
+piece6.mask = pygame.mask.from_surface(piece6.image)
+
+piece6b = Sprite()
+piece6b.image = pygame.image.load("piece6.png")
+piece6b.rect = piece6b.image.get_rect()
+piece6b.rect.topleft = (46, 68)
+piece6b.mask = pygame.mask.from_surface(piece6b.image)
+
+piece7 = Sprite()
+piece7.image = pygame.image.load("piece7.png")
+piece7.rect = piece7.image.get_rect()
+piece7.rect.topleft = (196, 46)
+piece7.mask = pygame.mask.from_surface(piece7.image)
+
+piece8 = Sprite()
+piece8.image = pygame.image.load("piece8.png")
+piece8.rect = piece8.image.get_rect()
+piece8.rect.topleft = (428, 192)
+piece8.mask = pygame.mask.from_surface(piece8.image)
+
+piece9 = Sprite()
+piece9.image = pygame.image.load("piece9.png")
+piece9.rect = piece9.image.get_rect()
+piece9.rect.topleft = (252, 46)
+piece9.mask = pygame.mask.from_surface(piece9.image)
+
+piece11 = Sprite()
+piece11.image = pygame.image.load("piece11.png")
+piece11.rect = piece11.image.get_rect()
+piece11.rect.topleft = (350, 48)
+piece11.mask = pygame.mask.from_surface(piece11.image)
+
+piece12 = Sprite()
+piece12.image = pygame.image.load("piece12.png")
+piece12.rect = piece12.image.get_rect()
+piece12.rect.topleft = (48, 48)
+piece12.mask = pygame.mask.from_surface(piece12.image)
+
+piece13 = Sprite()
+piece13.image = pygame.image.load("piece13.png")
+piece13.rect = piece13.image.get_rect()
+piece13.rect.topleft = (154, 192)
+piece13.mask = pygame.mask.from_surface(piece13.image)
+
+piece14 = Sprite()
+piece14.image = pygame.image.load("piece14.png")
+piece14.rect = piece14.image.get_rect()
+piece14.rect.topleft = (154, 320)
+piece14.mask = pygame.mask.from_surface(piece14.image)
+
+piece15 = Sprite()
+piece15.image = pygame.image.load("piece15.png")
+piece15.rect = piece15.image.get_rect()
+piece15.rect.topleft = (464, 126)
+piece15.mask = pygame.mask.from_surface(piece15.image)
+
+piece16 = Sprite()
+piece16.image = pygame.image.load("piece16.png")
+piece16.rect = piece16.image.get_rect()
+piece16.rect.topleft = (446, 48)
+piece16.mask = pygame.mask.from_surface(piece16.image)
+
+piece18 = Sprite()
+piece18.image = pygame.image.load("piece18.png")
+piece18.rect = piece18.image.get_rect()
+piece18.rect.topleft = (370, 126)
+piece18.mask = pygame.mask.from_surface(piece18.image)
+
+obstacles_group = pygame.sprite.Group()
+obstacles_group.add(ch, cb, start, piece2, piece6, piece6b, piece7,
+                    piece8, piece9, piece11, piece12, piece13, piece14, piece15, piece16, piece18)
+
+
+def collideObstacles(sprite):
+    bool = False
+    for sprites in obstacles_group.sprites():
+        if pygame.sprite.collide_mask(sprite, sprites):
+            bool = True
+    return bool
+
+
+perso = Player((288, 270), pygame.image.load("perso.png"))
+perso.mask = pygame.mask.from_surface(perso.image)
 player_group = pygame.sprite.Group()
 player_group.add(perso)
 
-perso1 = Sprite()
-perso1.image = pygame.image.load("fantome.png")
-perso1.rect = perso1.image.get_rect()
-perso1.rect.topleft = (50, 50)
+ghost = Sprite()
+ghost.image = pygame.image.load("fantome.png")
+ghost.rect = ghost.image.get_rect()
+ghost.rect.topleft = (15, 150)
+ghost.mask = pygame.mask.from_surface(ghost.image)
+ghosts = pygame.sprite.Group()
+ghosts.add(ghost)
+
 pygame.display.flip()
 pygame.key.set_repeat(10)
 
-ghosts = pygame.sprite.Group()
-ghosts.add(perso1)
-
 continuer = 1
 while continuer:
-    fenetre.blit(background, (0, 0))
+    fenetre.fill([0, 0, 0])
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_UP:
-                new_rect = perso.rect.move(0, -2)
-                if not collisionBackground(new_rect, perso1.rect):
-                    perso.rect = new_rect
+                perso.rect = perso.rect.move(0, -2)
+                if collisionRadius(perso.rect, ghost.rect) or collideObstacles(perso):
+                    perso.rect = perso.rect.move(0, 2)
             if event.key == K_DOWN:
-                new_rect = perso.rect.move(0, 2)
-                if not collisionBackground(new_rect, perso1.rect):
-                    perso.rect = new_rect
+                perso.rect = perso.rect.move(0, 2)
+                if collisionRadius(perso.rect, ghost.rect) or collideObstacles(perso):
+                    perso.rect = perso.rect.move(0, -2)
             if event.key == K_LEFT:
-                new_rect = perso.rect.move(-2, 0)
-                if not collisionBackground(new_rect, perso1.rect):
-                    perso.rect = new_rect
+                perso.rect = perso.rect.move(-2, 0)
+                if collisionRadius(perso.rect, ghost.rect) or collideObstacles(perso):
+                    perso.rect = perso.rect.move(2, 0)
             if event.key == K_RIGHT:
-                new_rect = perso.rect.move(2, 0)
-                if not collisionBackground(new_rect, perso1.rect):
-                    perso.rect = new_rect
+                perso.rect = perso.rect.move(2, 0)
+                if collisionRadius(perso.rect, ghost.rect) or collideObstacles(perso):
+                    perso.rect = perso.rect.move(-2, 0)
         if event.type == QUIT:
             continuer = 0
+    obstacles_group.draw(fenetre)
     player_group.draw(fenetre)
     ghosts.draw(fenetre)
-    pygame.display.update()
+    pygame.display.flip()
